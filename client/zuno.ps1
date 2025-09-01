1ffee4e878fe6bd2ef237738d5a589a67352c6fb
Stephen Tracey <51037643+StephenTracey@users.noreply.github.com>
2025-09-01 23:35:14 +1000

    Success

--- client/zuno.ps1
+++ client/zuno.ps1
@@ -1,29 +1,40 @@
 $endpoint = "https://api.zunobackup.com/get.php"
 $token = "ed906d935bcaf857f6f5c85e223495339279f6d45e973b5a71276bea3cbe741c"
 
-# Define where to save the file
-$downloadPath = "C:\zuno\pending.bin"
-
-# Make the request and save the file
 try {
-    Invoke-WebRequest `
+    $response = Invoke-WebRequest `
         -Uri $endpoint `
         -Method GET `
         -Headers @{ "Authorization" = "Bearer $token" } `
-        -OutFile $downloadPath `
         -ErrorAction Stop
+
+    # Extract filename from Content-Disposition header
+    $contentDisposition = $response.Headers["Content-Disposition"]
+    $filename = if ($contentDisposition -match 'filename="([^"]+)"') { $matches[1] } else { "pending.bin" }
+
+    # Define full path
+    $downloadPath = "C:\zuno\$filename"
+
+    # Save file manually
+    $response.RawContentStream.Position = 0
+    $reader = New-Object System.IO.BinaryReader($response.RawContentStream)
+    [System.IO.File]::WriteAllBytes($downloadPath, $reader.ReadBytes($response.RawContentStream.Length))
 
     Write-Host "File saved to $downloadPath"
 }
-
-
 catch {
-    $statusCode = $_.Exception.Response.StatusCode.Value__
-    $body = $_.Exception.Response.GetResponseStream()
-    $reader = New-Object System.IO.StreamReader($body)
-    $errorText = $reader.ReadToEnd()
+    if ($_.Exception.Response) {
+        $statusCode = $_.Exception.Response.StatusCode.Value__
+        $body = $_.Exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($body)
+        $errorText = $reader.ReadToEnd()
 
-    Write-Warning "HTTP $statusCode"
-    Write-Host " Response body:" -ForegroundColor Yellow
-    Write-Host $errorText
+        Write-Warning "HTTP $statusCode"
+        Write-Host "Response body:"
+        Write-Host $errorText
+    } else {
+        Write-Host "Request failed before receiving a response"
+        Write-Host "Error message:"
+        Write-Host $_.Exception.Message
+    }
 }
